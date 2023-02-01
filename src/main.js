313b791f6d6230dd2eb8d034d987ba25004929c6
Eduardo López <eduardo.lopezb@vasslatam.com>
2023-01-31 21:15:16 -0600

    Agregando nuevos modulos al proyecto

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 // El @ representa el directorio source.
 import "@/assets/css/tailwind.css"
+import Chart from 'chart.js'
+import Chartick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
+
 import router from "@/router"
 import { dollarfilter, percentFilter } from '@/filters'
+
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
 
 Vue.filter('dollar', dollarfilter)
 Vue.filter('percent', percentFilter)
