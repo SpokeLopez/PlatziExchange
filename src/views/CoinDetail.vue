1eb7f8442c607ddedfe392ea9f899ff00e8952e6
Eduardo López <eduardo.lopezb@vasslatam.com>
2023-02-05 20:30:19 -0600

    Limpieza en campos de conversion

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -43,19 +43,6 @@
                         </li>
                     </ul>
                 </div>
-                <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
-                    <button
-                        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Cambiar</button>
-
-                    <div class="flex flex-row my-5">
-                        <label class="w-full" for="convertValue">
-                            <input id="convertValue" type="number"
-                                class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" />
-                        </label>
-                    </div>
-
-                    <span class="text-xl"></span>
-                </div>
             </div>
             <line-chart class="my-10" 
             :colors="['orange']"
